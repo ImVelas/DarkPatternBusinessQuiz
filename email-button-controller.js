99e6c25b53a58e35080745e0eba3ad141485dc7f
Eve Schuetze <esc-berlin@outlook.de>
2023-08-02 11:42:29 +0200

    feat: start page finished, all questions work

---
+++ email-button-controller.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+    var sendButton = document.querySelector("div.send-button-container input.send-button");
+    var optOutButton = document.querySelector("input.subtle-button");
+    var emailCheckbox = document.querySelector("input.email-checkbox");
+    var emailInput = document.querySelector("input#email");
+
+    var nameInput = document.querySelector("input#name");
+    var startButton = document.querySelector("form.form-group-inner .send-button");
+
+    nameInput.disabled = true;
+    startButton.disabled = true;
+
+    sendButton.addEventListener("click", emailButtonsClicked);
+    optOutButton.addEventListener("click", emailButtonsClicked);
+
+    function emailButtonsClicked () {
+        sendButton.disabled = true;
+        optOutButton.disabled = true;
+        emailCheckbox.disabled = true;
+        emailInput.disabled = true;
+
+        nameInput.disabled = false;
+        startButton.disabled = false;
+    }
+});
